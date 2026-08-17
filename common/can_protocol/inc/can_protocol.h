6faa57f4e334a65de81f486f657669efaae7835a
nyankowan <nyankowan5@gmail.com>
2026-08-17 13:30:20 +0900

    改定中

--- common/can_protocol/inc/can_protocol.h
+++ common/can_protocol/inc/can_protocol.h
@@ -2,17 +2,22 @@
 #define CAN_PROTOCOL_H
 #include <stdint.h>
 
-#define CAN_DLC 8 //MAX
+#define CAN_DLC_MAX 8 //MAX
 
 //Don't use ID 0x00
 #define CAN_ID_NumItems 6
 typedef enum{
-    CAN_ID_UPPER_HOMING=0x01,
-    CAN_ID_LOWER_HOMING=0x02,
-    CAN_ID_UPPER_HOMING_DONE=0x03,
-    CAN_ID_LOWER_HOMING_DONE=0x04,
-    CAN_ID_UPPER_ARM=0x100,
-    CAN_ID_LOWER_ARM=0x101,
+    CAN_ID_UPPER_HOMING_ACK=        0x010
+    CAN_ID_UPPER_HOMING=            0x011,
+    CAN_ID_UPPER_HOMING_ACK=        0x020
+    CAN_ID_LOWER_HOMING=            0x021,
+    CAN_ID_UPPER_HOMING_DONE_ACK=   0x015,
+    CAN_ID_UPPER_HOMING_DONE=       0x016,
+    CAN_ID_LOWER_HOMING_DONE_ACK=   0x025,
+    CAN_ID_LOWER_HOMING_DONE=       0x026,
+    CAN_ID_UPPER_ARM=               0x100,
+    CAN_ID_LOWER_ARM=               0x101,
+    CAN_ID_ASSEMBLE=                0x102,
 }can_id_t;
 
 typedef struct{
@@ -28,16 +33,14 @@ typedef struct{
             uint8_t         :4;
         };
     }hand;
-    uint8_t reserved[3]//6,7,8
-    
 }lower_arm_t;
 
 typedef struct{
     int16_t x;//1.2
     int16_t y;//3,4
     int16_t z;//5.6
-    int16_t reserved;//7,8
 }upper_arm_t;
+
 typedef union{
     uint8_t raw[8];
     lower_arm_t lower_arm;
