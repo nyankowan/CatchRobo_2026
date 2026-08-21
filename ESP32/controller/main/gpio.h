3de101be42076880e20206ee6e806d64dca8cd30
nyankowan <nyankowan5@gmial.com>
2026-08-21 11:07:53 +0900

    回路完成

--- ESP32/controller/main/gpio.h
+++ ESP32/controller/main/gpio.h
@@ -4,4 +4,12 @@
 #define CAN_TX_GPIO 21
 #define CAN_RX_GPIO 22
 
-#endif //GPIO_H
+#define CAN_STATUS_LED_GPIO                  25
+#define LOWER_ARM_STATUS_LED_GPIO           26
+#define UPPER_ARM_STATUS_LED_GPIO           27
+#define ROBOMAS_CONTROLLER_STATUS_LED_GPIO  32
+#define STATUS_LED_GPIO                     33
+#define CONTROLLER_1_LED_GPIO                4
+#define CONTROLLER_2_LED_GPIO               18
+
+#endif // GPIO_H
