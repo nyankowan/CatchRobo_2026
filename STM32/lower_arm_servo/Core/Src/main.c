80382326a13c69ece455e517ffd2bf2443395b70
nyankowan <nyankowan5@gmial.com>
2026-09-12 09:33:25 +0900

    バグ

--- STM32/lower_arm_servo/Core/Src/main.c
+++ STM32/lower_arm_servo/Core/Src/main.c
@@ -141,7 +141,7 @@ static void hand_fold(){
 */
 static uint32_t hand_state_to_pulse(hand_state_t *state){
   if(state == NULL)return 0;
-  if(state == lower_arm_left){
+  if(state == &lower_arm_left){
     switch(*state){
       case HAND_STATE_HOLD:  return SERVO_45;
       case HAND_STATE_CATCH: return SERVO_150;
